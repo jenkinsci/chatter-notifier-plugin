3b72cdd109e917e5d30364b355ac57a322c64346
Simon Fell <fellforce@gmail.com>
2011-02-17 20:39:41 -0800

    add copyright header to source files

--- src/main/java/com/pocketsoap/salesforce/soap/ChatterClient.java
+++ src/main/java/com/pocketsoap/salesforce/soap/ChatterClient.java
@@ -1,3 +1,24 @@
+// Copyright (c) 2011 Simon Fell
+//
+// Permission is hereby granted, free of charge, to any person obtaining a 
+// copy of this software and associated documentation files (the "Software"), 
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the 
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included 
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+// THE SOFTWARE.
+//
+
 package com.pocketsoap.salesforce.soap;
 
 import java.io.IOException;
@@ -138,7 +159,18 @@ public class ChatterClient {
 		}
 	}
 	
-	private RuntimeException handleSoapFault(XMLStreamReader r) {
-		return new RuntimeException("boomn");// TODO
+	// read a soap fault message and turn it into an exception
+	private RuntimeException handleSoapFault(XMLStreamReader rdr) throws XMLStreamException {
+		String fc = null, fs = null;
+		while (rdr.next() != XMLStreamReader.END_DOCUMENT) {
+			if (rdr.getEventType() == XMLStreamReader.START_ELEMENT) {
+				String ln = rdr.getLocalName();
+				if (ln.equals("faultcode")) 
+					fc = rdr.getElementText();
+				else if (ln.equals("faultstring"))
+					fs = rdr.getElementText();
+			}
+		}
+		return new SoapFaultException(fc, fs);
 	}
 }
